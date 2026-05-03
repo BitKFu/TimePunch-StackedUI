1e9b60c03d4e2fcac780d6bd8ed10222e643809f
Gerhard Stephan <gerhard.stephan@timepunch.de>
2026-05-03 06:06:04 +0200

    Fixed WinUI Build

--- .NET Core/TimePunch-WinUI-StackedUI-Demo/MainWindow.xaml.cs
+++ .NET Core/TimePunch-WinUI-StackedUI-Demo/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ using TimePunch_WinUI_StackedUI_Demo.ViewModels;
 
 namespace TimePunch_WinUI_StackedUI_Demo
 {
-    public sealed partial class MainWindow : UserControl
+    public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
